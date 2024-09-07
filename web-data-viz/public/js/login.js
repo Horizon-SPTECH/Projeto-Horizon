4fdede8d8b8adfa5244054d567267b119b9757e9
KauanPaschoal <kauan.psantos@sptech.school>
2024-09-07 15:29:56 -0300

    Criação da area de footer, alteração na animação do botão na tela inicial e inserção de show password em todas as telas

--- web-data-viz/public/js/login.js
+++ web-data-viz/public/js/login.js
@@ -1,8 +1,25 @@
-var nome = nome.value;
-var email = email.value;
-var senha = senha.value;
-var confirmarSenha = confSenha.value
+function verSenha() {
+    const senhaInput = document.getElementById('senha'); 
+    const olho = document.getElementById('viewSenha');
 
-if (nome.length < 0) {
-    
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text'; 
+        olho.src = 'assets/olho-fechado.png';
+    } else {
+        senhaInput.type = 'password';
+        olho.src = 'assets/olho.png';
+    }
+}
+
+function verSenh2() {
+    const senhaInput = document.getElementById('confirmacao_senha'); 
+    const olho = document.getElementById('viewSenha-confirmar');
+
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text'; 
+        olho.src = 'assets/olho-fechado.png';
+    } else {
+        senhaInput.type = 'password';
+        olho.src = 'assets/olho.png';
+    }
 }
