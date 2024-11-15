a20aa65f773c2a80b5d74d1d04b7f4a66a1c5adb
Igor <igor.jimenez@sptech.school>
2024-11-15 20:26:00 -0300

    finalizao das telas cadastro-usuario e editar perfil

---
+++ web-data-viz/public/js/disabled-senha.js
@@ -0,0 +1,7 @@
+function verSenha(inputId, imgElement) {
+    const input = document.getElementById(inputId);
+    const isPasswordVisible = input.type === "text";
+    input.type = isPasswordVisible ? "password" : "text";
+    imgElement.src = isPasswordVisible ? "assets/olho.png" : "assets/olho-fechado.png";
+    imgElement.alt = isPasswordVisible ? "Mostrar senha" : "Ocultar senha";
+}
