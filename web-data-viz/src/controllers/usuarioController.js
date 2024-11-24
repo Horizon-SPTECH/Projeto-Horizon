cb93398dd60bc3c16ebf4362d9f9233ce539d345
Igor <igor.jimenez@sptech.school>
2024-11-24 05:45:13 -0300

    AJUSTE PARA NÃO DAR CONFLITO

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -1,5 +1,17 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+
+
+const transporter = nodemailer.createTransport({
+    host:"smtp-relay.gmail.com",
+    service: 'gmail',
+    port:587,
+    auth: {
+        user: 'horizonContateNos@gmail.com',
+        pass: 'wxfg vjdw ybis qlmg'
+    }
+});
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -13,14 +25,15 @@ function autenticar(req, res) {
 
         usuarioModel.autenticar(email, senha)
             .then(
-                function (resultado) {
-                    console.log(`\nResultados encontrados: ${resultado.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
+                function (resultadoAutenticar) {
+                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-                    if (resultado.length == 1) {
-                        console.log(resultado);
-                        res.json(resultado[0]);
-                    } else if (resultado.length == 0) {
+                    if (resultadoAutenticar.length == 1) {
+                        console.log(resultadoAutenticar);
+
+
+                    } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
@@ -34,6 +47,7 @@ function autenticar(req, res) {
                 }
             );
     }
+
 }
 
 function cadastrar(req, res) {
@@ -41,10 +55,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var idTipoUsuario = req.body.idTipoUsuarioServer;
-    var idEmpresa = req.body.idEmpresaServer;
-    var telefone = req.body.telefoneServer;
-    var cpf = req.body.cpfServer;
+    var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -53,18 +64,12 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (idTipoUsuario == undefined) {
-        res.status(400).send("Seu tipo do usuário está undefined!");
-    } else if (idEmpresa == undefined){
-        res.status(400).send("Sua empresa está undefined!");
-    }else if (telefone == undefined) {
-        res.status(400).send("Seu telefone está undefined!");
-    }else if (cpf == undefined){
-        res.status(400).send("Seu cpf está undefined!");
-    }else{
+    } else if (fkEmpresa == undefined) {
+        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, cpf, senha, idTipoUsuario, idEmpresa, telefone)
+        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -82,10 +87,11 @@ function cadastrar(req, res) {
     }
 }
 
-function perfilUsuario(req, res) {
-    var idUsuario = req.params.idUsuario
-  
-    usuarioModel.perfilUsuario(idUsuario).then(function (resultado) {
+function listarFuncionarios(req, res) {
+
+    var idEMpresa = req.params.idEMpresa
+
+    usuarioModel.listarFuncionarios(idEMpresa).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -98,77 +104,130 @@ function perfilUsuario(req, res) {
     });
   }
 
-  function desativarFuncionario(req, res) {
-    var idUsuario = req.params.idUsuario
-  
-    usuarioModel.desativarFuncionario(idUsuario).then(function (resultado) {
-       res.json(resultado);
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar a quantidade de avisos: ", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
-    });
-  }
 
-  function atualizarUsuario(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+
+
+function desativarUsuario(req, res){
+    var idUser = req.params.idUser;
+
+    usuarioModel.desativarUsuario(idUser)
+    .then(
+        function(resultado){
+            res.json(resultado);
+        }
+    )
+    .catch(
+        function(erro){
+            console.log(erro);
+            console.log("ERRO ao atualizar com put", erro.sqlMessage)
+            res.status(500).json(erro.sqlMessage);
+        }
+    )
+}
+
+
+
+
+
+function gerarToken(req, res) {
+    const email = req.body.emailServer;
+
+    if (!email) {
+        res.status(400).send("Email não informado!");
+        return;
+    }
+
+    usuarioModel.buscarPorEmail(email).then((resultado) => {
+        if (resultado.length === 0) {
+          res.status(404).send("Email não encontrado!");
+        } else {
+          const token = crypto.randomBytes(64).toString("hex");
     
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var idUsuario = req.body.idUsuarioServer;
-    var telefone = req.body.telefoneServer;
-
-    // Faça as validações dos valores
-     if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (idUsuario == undefined) {
-        res.status(400).send("Seu tipo do usuário está undefined!");
-    } else if (telefone == undefined) {
-        res.status(400).send("Seu telefone está undefined!");
-    } else{
-
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.atualizarUsuario(email, senha, idUsuario, telefone)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
+          usuarioModel.salvarToken(email, token).then(() => {
+            let mailOptions = {
+              from: 'horizonContateNos@gmail.com', 
+              to: email,
+              subject: 'Recuperação de Senha - Token', 
+              text: `Olá, você solicitou a recuperação de senha. Use o seguinte token para prosseguir: ${token}`, 
+            };
+    
+            transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                console.error("Erro ao enviar e-mail:", error);
+                res.status(500).send("Erro ao enviar o e-mail com o token.");
+                return;
+              }
+              console.log('E-mail enviado:', info.response);
+              res.status(200).send(`Token gerado e enviado para o e-mail: ${email}`);
+            });
+          }).catch((erro) => {
+            res.status(500).send("Erro ao salvar token.");
+            console.error(erro);
+          });
+        }
+      }).catch((erro) => {
+        res.status(500).send("Erro ao buscar email no banco.");
+        console.error(erro);
+      });
 }
 
-function verificarSenha(req, res) {
-    var idUsuario = req.params.idUsuario
+function verificarToken(req, res) {
+    const token = req.body.tokenServer;
 
-    usuarioModel.verificarSenha(idUsuario).then(function (resultado) {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
+    if (!token) {
+        res.status(400).send("Token não informado!");
+        return;
+    }
+
+    usuarioModel.verificarToken(token).then((resultado) => {
+        if (resultado.length === 0) {
+            res.status(404).send("Token inválido ou expirado!");
         } else {
-            res.status(204).send("Nenhum resultado encontrado!")
+            res.status(200).send("Token válido! Redirecionando para alteração de senha...");
         }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar a quantidade de avisos: ", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
+    }).catch((erro) => {
+        res.status(500).send("Erro ao verificar token no banco.");
+        console.error(erro);
     });
-  }
+}
+
+function alterarSenha(req, res) {
+    const { tokenServer: token, senhaNovaServer: novaSenha } = req.body;
+
+    if (!token || !novaSenha) {
+        res.status(400).send("Token ou nova senha não informados!");
+        return;
+    }
+
+    usuarioModel.verificarToken(token).then((resultado) => {
+        if (resultado.length === 0) {
+            res.status(404).send("Token inválido ou expirado!");
+        } else {
+            usuarioModel.alterarSenha(resultado[0].email, novaSenha).then(() => {
+                const novoToken = crypto.randomBytes(64).toString("hex");
+                usuarioModel.salvarToken(resultado[0].email, novoToken).then(() => {
+                    res.status(200).send("Senha alterada com sucesso!");
+                }).catch((erro) => {
+                    res.status(500).send("Erro ao salvar novo token no banco.");
+                    console.error(erro);
+                });
+            }).catch((erro) => {
+                res.status(500).send("Erro ao alterar senha.");
+                console.error(erro);
+            });
+        }
+    }).catch((erro) => {
+        res.status(500).send("Erro ao verificar token.");
+        console.error(erro);
+    });
+}
 
 module.exports = {
     autenticar,
     cadastrar,
-    perfilUsuario,
-    desativarFuncionario,
-    atualizarUsuario,
-    verificarSenha
+    listarFuncionarios,
+    desativarUsuario,
+    gerarToken,
+    verificarToken,
+    alterarSenha
 }
