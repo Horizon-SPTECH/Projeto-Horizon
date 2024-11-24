67b4cf48c8358c24ec9b3b546b949cddee816d8d
Igor <igor.jimenez@sptech.school>
2024-11-24 18:31:38 -0300

    juntando login, com a dashboard e parametros

--- web-data-viz/public/js/block-screan.js
+++ web-data-viz/public/js/block-screan.js
@@ -1,12 +1,32 @@
 function acessar(){
 
     var ativo = sessionStorage.getItem("ATIVO");
+    var ativoEmpresa = sessionStorage.getItem("ATIVO_EMPRESA");
 
-    if(ativo == null || ativo == 0){
+    if(ativo == null || ativo == 0 || ativoEmpresa == null || ativoEmpresa == 0){
        window.location.href = "login.html"; 
     }
 }
 
+
+function cargo(){
+    var tipocargo = sessionStorage.getItem("ID_TIPO");
+
+    if(tipocargo == 1){
+        Swal.fire({
+            icon: 'error',
+            title: 'Erro',
+            text: 'Esta conta não tem acesso a essa página!',
+            showConfirmButton: false
+        });
+    
+        setTimeout(function() {
+            window.location.href = "dashboard.html";
+        }, 2000);
+    }
+
+   
+}
 
 function sessao(){
     var email = sessionStorage.getItem("EMAIL");
@@ -19,8 +39,6 @@ function sessao(){
 
 function sessaoEmail(){
     var email = sessionStorage.getItem("EMAIL");
-
-
     if(email == null){
         window.location.href = "login.html";
     }
--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -2,8 +2,22 @@ var database = require("../database/config")
 
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-    var instrucaoSql = `
-        SELECT id, nome, email, ativo, cpf, senha, id_empresa, id_tipo_usuario FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    var instrucaoSql = `        
+        SELECT 
+            u.id, 
+            u.nome, 
+            u.email, 
+            u.ativo,
+            u.cpf, 
+            u.senha, 
+            u.id_empresa, 
+            u.id_tipo_usuario,
+            e.ativo AS empresa_ativo
+        FROM 
+            usuario u
+        INNER JOIN 
+            empresa e ON u.id_empresa = e.id
+        WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
