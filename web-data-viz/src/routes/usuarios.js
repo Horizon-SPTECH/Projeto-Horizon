ef41fe670da945fb63fcda6134d441cd294a1b8d
Igor <igor.jimenez@sptech.school>
2024-11-24 05:38:24 -0300

    AJUSTE PARA NÃO DAR CONFLITO

--- web-data-viz/src/routes/usuarios.js
+++ web-data-viz/src/routes/usuarios.js
@@ -4,31 +4,32 @@ var router = express.Router();
 var usuarioController = require("../controllers/usuarioController");
 
 //Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
-router.post("/cadastrar/:idEmpresa", function (req, res) {
+router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
-
 })
 
-//Recebendo os dados do html e direcionando para a função autenticar de usuarioController.js
 router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
 });
 
-//Recebendo os dados do html e direcionando para a função autenticar de usuarioController.js
-router.get("/perfilUsuario/:idUsuario", function (req, res) {
-    usuarioController.perfilUsuario(req, res);
+router.get("/listarFuncionarios/:idEMpresa", function(req, res){
+    usuarioController.listarFuncionarios(req, res);
 });
 
-router.put("/desativarFuncionario/:idUsuario", function (req, res) {
-    usuarioController.desativarFuncionario(req, res);
+router.put("/desativarUsuario/:idUser", function(req, res){
+    usuarioController.desativarUsuario(req, res);
 });
 
-router.put("/atualizarUsuario/:idUsuario", function (req, res) {
-    usuarioController.atualizarUsuario(req, res);
+router.post("/gerarToken", function (req, res) {
+    usuarioController.gerarToken(req, res);
 });
 
-router.get("/verificarSenha/:idUsuario", function (req, res) {
-    usuarioController.verificarSenha(req, res);
+router.post("/verificarToken", function (req, res) {
+    usuarioController.verificarToken(req, res);
+});
+
+router.post("/alterarSenha", function (req, res) {
+    usuarioController.alterarSenha(req, res);
 });
 
 
